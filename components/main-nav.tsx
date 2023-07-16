b3476df1f52de4347f096c07dd2d953d599a309b
SAFWAT <safwathkhan2@gmail.com>
2023-07-17 02:25:48 +0530

    changes in navbar

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -18,8 +18,6 @@ const MainNav: React.FC<MainNavProps> = ({ data }) => {
     label: route.name,
     active: pathname === `/category/${route.id}`,
   }));
-  console.log(routes);
-  
 
   return (
     <nav className="mx-6 flex items-center space-x-4 lg:space-x-6">
--- components/navbar-actions.tsx
+++ components/navbar-actions.tsx
@@ -1,10 +1,34 @@
 'use client'
 
+import { useRouter } from "next/navigation"
+import { useEffect, useState } from "react"
+import { ShoppingBag } from "lucide-react";
+
+import Button  from "./ui/button"
+
 
 const NavbarActions = () => {
 
+    const [isMounted, setIsMounted] = useState(false)
+
+    useEffect(() => {
+        setIsMounted(true)
+    }, [])
+
+    const router = useRouter()
+
+
+    if (!isMounted) return null
+
     return(
-        <div className="flex ml-auto gap-x-2">hello </div>
+        <div className="ml-auto flex items-center gap-x-4">
+            <Button
+                onClick={() => router.push('/cart')}
+                className="flex items-center rounded-full bg-black px-4 py-2"
+            >
+                <ShoppingBag className="w-6 h-6 text-white" />
+            </Button>
+        </div>
     )
 }
 
