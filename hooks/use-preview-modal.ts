858f0f6764f608b505a8d6884461447dde9e3156
SAFWAT <safwathkhan2@gmail.com>
2023-07-18 05:19:15 +0530

    added preview mode

--- hooks/use-preview-modal.ts
+++ hooks/use-preview-modal.ts
@@ -2,19 +2,18 @@ import { create } from "zustand";
 
 import { Product } from "@/types";
 
-
 interface PreviewModalStore {
-    isOpen: boolean;
-    data?: Product;
-    onOpen: (data: Product) => void;
-    onClose: () => void;
+  isOpen: boolean;
+  data?: Product;
+  onOpen: (data: Product) => void;
+  onClose: () => void;
 }
 
-const UsePreviewModal = create<PreviewModalStore>((set) => ({
-    isOpen: false,
-    data: undefined,
-    onOpen: (data: Product) => set({ isOpen: true, data }),
-    onClose: () => set({ isOpen: false}),
-}))
+const usePreviewModal = create<PreviewModalStore>((set) => ({
+  isOpen: false,
+  data: undefined,
+  onOpen: (data: Product) => set({ isOpen: true, data }),
+  onClose: () => set({ isOpen: false }),
+}));
 
-export default UsePreviewModal
+export default usePreviewModal;
